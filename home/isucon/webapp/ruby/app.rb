e394a689c2c540506beafca67f6ee302ebe98862
Ochi-Shoichi5539 <dekamintv@gmail.com>
2023-11-25 12:56:47 +0900

    tag_idカラムのみ取得するようにする

--- home/isucon/webapp/ruby/app.rb
+++ home/isucon/webapp/ruby/app.rb
@@ -106,7 +106,7 @@ module Isupipe
         owner_model = tx.xquery('SELECT * FROM users WHERE id = ?', livestream_model.fetch(:user_id)).first
         owner = fill_user_response(tx, owner_model)
 
-        tags = tx.xquery('SELECT * FROM livestream_tags WHERE livestream_id = ?', livestream_model.fetch(:id)).map do |livestream_tag_model|
+        tags = tx.xquery('SELECT tag_id FROM livestream_tags WHERE livestream_id = ?', livestream_model.fetch(:id)).map do |livestream_tag_model|
           tag_model = tx.xquery('SELECT * FROM tags WHERE id = ?', livestream_tag_model.fetch(:tag_id)).first
           {
             id: tag_model.fetch(:id),
